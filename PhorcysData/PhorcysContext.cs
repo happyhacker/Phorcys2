798498b8acdf6f6d802e3869067c74df9496d9e3
Larry Hack <Larry@Hacksoft.net>
2024-01-24 18:35:00 -0600

    Changed connectionstring to use sheckexley and to trust local certificate

--- PhorcysData/PhorcysContext.cs
+++ PhorcysData/PhorcysContext.cs
@@ -12,9 +12,9 @@ namespace Phorcys.Data;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=HACKSOFT\\MSSQLSERVER01;Initial Catalog=SCUBA;Integrated Security=SSPI;Encrypt=False;")
+        //optionsBuilder.UseSqlServer("Data Source=HACKSOFT\\MSSQLSERVER01;Initial Catalog=SCUBA;Integrated Security=SSPI;Encrypt=False;")
+        optionsBuilder.UseSqlServer("Data Source=HACKSOFT\\MSSQLSERVER01;Initial Catalog=SCUBA;User Id=sheckexley;password=Measureless2Man;TrustServerCertificate=True;")
              .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
  
     }
 }
-clear
