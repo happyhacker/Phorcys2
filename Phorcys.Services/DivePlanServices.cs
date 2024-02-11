ebe47852ca38c6a046d231dbcfc3b9641750be7a
Larry Hack <Larry@Hacksoft.net>
2024-02-11 13:57:17 -0600

    Adding files

---
+++ Phorcys.Services/DivePlanServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Phorcys.Data;
+using Phorcys.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phorcys.Services;
+
+public class DivePlanServices
+{
+	PhorcysContext context = new PhorcysContext();
+
+	public IEnumerable<DivePlan> GetDivePlans()
+	{
+		var divePlans = context.DivePlans.Include(d => d.DiveSite).ThenInclude(u => u.User).OrderByDescending(dp => dp.ScheduledTime).ToList();
+		return divePlans;
+	}
+
+}
