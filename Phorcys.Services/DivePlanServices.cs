7c3516cc3d4d021faf1e560d6cbf5f05c4e49297
Larry Hack <Larry@Hacksoft.net>
2024-03-07 20:58:07 -0600

    Added Dive Plan Index Page and Delete Dive Plan

--- Phorcys.Services/DivePlanServices.cs
+++ Phorcys.Services/DivePlanServices.cs
@@ -19,4 +19,28 @@ public class DivePlanServices
 		return divePlans;
 	}
 
+	public async Task<IEnumerable<DivePlan>> GetDivePlansAsync()
+	{
+		try
+		{
+			var divePlans = context.DivePlans.Include(d => d.DiveSite).ThenInclude(u => u.User).AsNoTracking().OrderByDescending(dp => dp.ScheduledTime).ToList();
+			return divePlans;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+			throw new Exception("Can't connect to database");
+		}
+	}
+
+	public void Delete(int id)
+	{
+		var divePlan = context.DivePlans.Find(id);
+		if (divePlan != null)
+		{
+			context.DivePlans.Remove(divePlan);
+			context.SaveChanges();
+		}
+	}
+
 }
