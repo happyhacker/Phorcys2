149a97cc47b7ecdfd541cf1a40ac6bae7e9c0954
Larry Hack <Larry@Hacksoft.net>
2024-03-31 20:27:25 -0500

    created new files

---
+++ Phorcys.Services/DiveSiteServices.cs
@@ -0,0 +1,36 @@
+﻿using Phorcys.Data;
+using Phorcys.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phorcys.Services
+{
+
+	public class DiveSiteServices
+	{
+	PhorcysContext context = new PhorcysContext();
+	public IEnumerable<DiveSite> GetDiveSites()
+		{
+			try
+			{
+				//var diveSites = context.DiveSites.Include(d => d.DiveSiteId).ThenInclude(u => u.User).AsNoTracking().OrderByDescending(dp => dp.ScheduledTime).ToList();
+				var diveSites = new List<DiveSite>
+				{
+					new DiveSite {Title = "Site 1", DiveSiteId = 1},
+					new DiveSite {Title = "Site 2", DiveSiteId = 2}
+				};
+				
+				return diveSites;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				throw new Exception("Can't connect to database");
+			}
+		}
+
+	}
+}
---
+++ Phorcys2Web/Models/DivePlanViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Phorcys.Domain;
+using System.ComponentModel;
+
+namespace Phorcys.Web.Models
+{
+	public class DivePlanViewModel
+	{
+		public int DivePlanId { get; set; }
+		public int? DiveSiteId { get; set; }
+		public int? DiveSiteSelectedId { get; set; }
+		public IList<SelectListItem> DiveSiteList { get; set; }
+
+		[DisplayName("Title")]
+		public string Title { get; set; } = null!;
+		[DisplayName("Minutes")]
+		public int? Minutes { get; set; }
+		[DisplayName("Scheduled Time")]
+		public DateTime ScheduledTime { get; set; }
+		[DisplayName("Max Depth")]
+		public int? MaxDepth { get; set; }
+		[DisplayName("Notes")]
+		public string? Notes { get; set; }
+		public int UserId { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime LastModified { get; set; }
+		public virtual DiveSite? DiveSite { get; set; }
+
+		//public virtual ICollection<TanksOnDive> TanksOnDives { get; set; } = new List<TanksOnDive>();
+
+		//public required User User { get; set; }
+	}
+}
