3db4a273884a68ca5678f8703c0cfbd528ccca47
Larry Hack <Larry@Hacksoft.net>
2024-04-14 21:24:58 -0500

    Modified Program.cs to include Razor pages

--- Phorcys2Web/Program.cs
+++ Phorcys2Web/Program.cs
@@ -37,6 +37,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.MapRazorPages();
+//app.MapControllers();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
