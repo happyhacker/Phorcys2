dd463e60cb0c84e974b831cea3c624b032d063fa
Larry Hack <Larry@Hacksoft.net>
2024-05-03 00:58:51 -0500

    Added favicon.ico to show on browser tab

---
+++ Phorcys2Web/Views/Shared/LogOnUserControl.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Phorcys.Web.Views.Shared
+{
+    public class LogOnUserControlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
