dfcb12cdeb3fa06b662c4b9f08ed093a872e898c
Larry Hack <Larry@Hacksoft.net>
2024-07-17 07:03:35 -0500

    Added Create for MyCertifications

---
+++ Phorcys2Web/Models/MyCertificationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Phorcys.Web.Models
+{
+	public class MyCertificationViewModel
+	{
+		public int DiverCertificationId { get; set; }
+		[Required]
+		public int DiveAgencyId { get; set; }
+		public int DiverId { get; set; }
+		[Required]
+		public int CertificationId { get; set; }
+		public DateTime? Certified { get; set; }
+		public string CertificationNum { get; set; }
+		public string Notes { get; set; }
+		public DateTime Created { get; set; }
+		public int InstructorId { get; set; }
+
+		public IList<SelectListItem> DiveAgencyListItems { get; set; }
+		public IList<SelectListItem> CertificationListItems { get; set; }
+		public IList<SelectListItem> InstructorListItems { get; set; }
+		public SelectList InstructorSelectList { get; set; }
+
+	}
+}
