8b6c23f243a44c58f6384e8a720f107cade8c15a
Larry Hack <Larry@hacksoft.net>
2024-11-21 00:16:29 -0600

    Resend email confirmation

--- Phorcys2Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ Phorcys2Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -1,17 +1,23 @@
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Phorcys.Web.Areas.Identity.Pages.Account
 {
     public class ResendEmailConfirmationModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
 
-        public ResendEmailConfirmationModel(UserManager<IdentityUser> userManager)
+        public ResendEmailConfirmationModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
         {
             _userManager = userManager;
+            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -42,14 +48,21 @@ namespace Phorcys.Web.Areas.Identity.Pages.Account
                 return Page();
             }
 
+            // Generate the email confirmation token
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+            // Generate the email confirmation link
             var callbackUrl = Url.Page(
                 "/Account/ConfirmEmail",
                 pageHandler: null,
-                values: new { userId = user.Id, code = code },
+                values: new { area = "Identity", userId = user.Id, code = code },
                 protocol: Request.Scheme);
 
             // Send the confirmation email
+            await _emailSender.SendEmailAsync(Email, "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
             TempData["Message"] = "Confirmation email sent. Please check your inbox.";
             return RedirectToPage("/Account/Login");
         }
