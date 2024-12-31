747eb7f179476743fe101e8735f2664d162fdac4
Larry Hack <Larry@hacksoft.net>
2024-12-30 20:39:03 -0600

    Added 1st xUnit test

---
+++ Phorcys.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phorcys.Services.Utilities;
+using Xunit;
+
+namespace Phorcys.Tests
+{
+	public class CalculatorTests
+	{
+		[Fact]
+		public void Add_ReturnsCorrectSum()
+		{
+			// Arrange
+			var calculator = new Calculator();
+
+			// Act
+			var result = calculator.Add(2, 3);
+
+			// Assert
+			Assert.Equal(5, result);
+		}
+	}
+}
