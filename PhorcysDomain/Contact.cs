39963d7bc87f5cf124638f5d5c8e17ef869f73e7
Larry Hack <Larry@hacksoft.net>
2025-09-02 23:29:41 -0500

    Fixed bug for LSB-2417 crash when trying to display contacts

--- PhorcysDomain/Contact.cs
+++ PhorcysDomain/Contact.cs
@@ -6,21 +6,21 @@ namespace Phorcys.Domain;
 public partial class Contact
 {
     public int ContactId { get; set; }
-    public string Company { get; set; } = null!;
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Address1 { get; set; } = null!;
-    public string Address2 { get; set; } = null!;
-    public string City { get; set; } = null!;
-    public string State { get; set; } = null!;
-    public string PostalCode { get; set; } = null!;
+    public string? Company { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Address1 { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
     public string? CountryCode { get; set; }
-    public string Email { get; set; } = null!;
-    public string CellPhone { get; set; } = null!;
-    public string HomePhone { get; set; } = null!;
-    public string WorkPhone { get; set; } = null!;
+    public string? Email { get; set; }
+    public string? CellPhone { get; set; }
+    public string? HomePhone { get; set; }
+    public string? WorkPhone { get; set; }
     public DateTime? Birthday { get; set; }
-    public string Gender { get; set; } = null!;
+    public string? Gender { get; set; }
     public string? Notes { get; set; }
     public int UserId { get; set; }
     public DateTime Created { get; set; }
